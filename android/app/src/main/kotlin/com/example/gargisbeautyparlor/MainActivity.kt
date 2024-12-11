bbadbbf86b74ec17ce9a8a96364f2fe9c6317c5f
Pratiksha-ai-06 <bhosale61098@gmail.com>
2024-12-11 06:49:07 -0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/gargisbeautyparlor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gargisbeautyparlor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/app_color.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ColorsForApp {
+  ColorsForApp._();
+  // Theme Color
+  static Color primaryButtonColor=const Color(0xff2164f3);
+  static Color buttonTextColor=const Color(0xffffffff);
+  static Color blackTextColor=const Color(0xff000000);
+
+
+//App color
+
+  static Color textFieldColor=const Color(0xfff2f2f2);
+  static Color pendingTextColor=const Color(0xffFF00B2);
+  static Color scheduledTextColor=const Color(0xffFF6F01);
+  static Color acceptTextColor=const Color(0xff08AE6E);
+  static Color cancelTextColor=const Color(0xffFF666A);
+  static Color blueTextColor=const Color(0xff77a0f7);
+  static Color blueLightTextColor=const Color(0xffaac3f7);
+
+  static Color blackLightTextColor=const Color(0xffEAEAEA);
+  static Color serviceCircleColor=const Color(0xffE1F5FA);
+  static Color serviceIconColor=const Color(0xff156778);
+  static Color shareInviteCodeButtonColor=const Color(0xff1854AF);
+  static Color offerIconColor=const Color(0xff3BFB00);
+  static Color offerButtonColor=const Color(0xffD3D3D3);
+  static Color notificationIconColor=const Color(0xffF32F2F);
+  static Color calenderColor=const Color(0xffE9F0FE);
+
+  //review screen
+  static Color starReviewColor=const Color(0xffFEDC00);
+  static Color commentColor=const Color(0xff9B4545);
+  static Color nameColor=const Color(0xff6B0606);
+  static Color blackStarColor=const Color(0xffA9A5A5);
+
+
+
+
+
+
+}
